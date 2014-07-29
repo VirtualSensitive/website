442e6ed8b08b223b9b14fa6f219a4b44acda214a
Flo-Schield-Bobby <florent.schildknecht@gmail.com>
2014-07-29 17:37:52 +0200

    Register animation styles and scripts on each page

--- wp-content/themes/federal-child/functions.php
+++ wp-content/themes/federal-child/functions.php
@@ -171,18 +171,16 @@ function rb_enqueue_scripts(){
 		'homeURL'=>home_url(),
 	));
 
-	if (is_page('home')) {
-		wp_register_script('underscore', get_template_directory_uri() . '/js/underscore-1.6.0.js', array(), null, true);
-		wp_register_script('tweenmax', get_template_directory_uri() . '/js/tweenmax-1.12.1.min.js', array(), null, true);
-		wp_register_script('jquery-gsap', get_template_directory_uri() . '/js/jquery.gsap-0.1.8.min.js', array(), null, true);
-		wp_register_script('ws-animation', get_template_directory_uri() . '/js/ws-animation-1.0.0.min.js', array(
-			'underscore',
-			'tweenmax',
-			'jquery-gsap'
-		), null, true);
-		wp_enqueue_script('ws-animation');
-		wp_enqueue_style('ws-animation',  get_template_directory_uri() . '/css/ws-animation.min.css', false, null, 'all');
-	}
+	wp_register_script('underscore', get_template_directory_uri() . '/js/underscore-1.6.0.js', array(), null, true);
+	wp_register_script('tweenmax', get_template_directory_uri() . '/js/tweenmax-1.12.1.min.js', array(), null, true);
+	wp_register_script('jquery-gsap', get_template_directory_uri() . '/js/jquery.gsap-0.1.8.min.js', array(), null, true);
+	wp_register_script('ws-animation', get_template_directory_uri() . '/js/ws-animation-1.0.0.min.js', array(
+		'underscore',
+		'tweenmax',
+		'jquery-gsap'
+	), null, true);
+	wp_enqueue_script('ws-animation');
+	wp_enqueue_style('ws-animation',  get_template_directory_uri() . '/css/ws-animation.min.css', false, null, 'all');
 }
 
 function rb_admin_enqueue_scripts(){
